cca0cadcf8656fbdbfaeacfb5359d8c9c3881cbe
Corey Ballou <corey@coreyballou.com>
2013-03-02 06:53:27 -0500

    Removed technical debt due to nested if/elseif
    Removed nested if/elseif statements where possible. Added in $.errorHandler.message to allow the user to override the default message format. Drastically cut down on character count by creating one message.

--- jquery.errorHandler.js
+++ jquery.errorHandler.js
@@ -4,62 +4,76 @@
  */
 if (jQuery) (function($) {
     $.errorHandler = function(err, form) {
+        var message,
+            $field,
+            placement,
+            fld;
+            
         err = (err == undefined || err == '') ? {} : err;
         for (var name in err) {
-            var message = err[name];
-            var $field = form && form.length ? $('#' + form).find('#' + name) : $('#' + name);
-            if (!$field.length) $field = form && form.length ? $('#' + form).find('*[name=' + name + ']') : $('*[name=' + name + ']');
-            if ($field.length) {
-                $field.addClass('error');
-                var placement = $field.data('placement');
-                if (placement) {
-                    if (placement.indexOf('appendClosest') > -1) {
-                        var fld = placement.match(/appendClosest\(([a-zA-Z0-9_\-.#]+)\)/);
-                        if (fld && fld.length) {
-                            $field.closest(fld[1]).append('<span class="error"><span>' + message + '</span></span>');
-                        } else {
-                            $field.parent().append('<span class="error"><span>' + message + '</span></span>');
-                        }
-                    } else if (placement.indexOf('prependClosest') > -1) {
-                        var fld = placement.match(/prependClosest\(([a-zA-Z0-9_\-.#]+)\)/);
-                        if (fld && fld.length) {
-                            $field.closest(fld[1]).prepend('<span class="error"><span>' + message + '</span></span>');
-                        } else {
-                            $field.parent().prepend('<span class="error"><span>' + message + '</span></span>');
-                        }
-                    } else if (placement == 'appendForm') {
-                        $field.closest('form').append('<span class="error"><span>' + message + '</span></span>');
-                    } else if (placement == 'prependForm') {
-                        $field.closest('form').prepend('<span class="error"><span>' + message + '</span></span>');
-                    } else if (placement == 'appendSiblingLabel') {
-                        $field.siblings('label').append('<span class="error"><span>' + message + '</span></span>');
-                    } else if (placement == 'prependSiblingLabel') {
-                        $field.siblings('label').prepend('<span class="error"><span>' + message + '</span></span>');
-                    } else if (placement.indexOf('appendParent') > -1) {
-                        var fld = placement.match(/appendParent\(([a-zA-Z0-9_\-.#]+)\)/);
-                        if (fld && fld.length) {
-                            $field.parent(fld[1]).append('<span class="error"><span>' + message + '</span></span>');
-                        } else {
-                            $field.parent().append('<span class="error"><span>' + message + '</span></span>');
-                        }
-                    } else if (placement.indexOf('prependParent') > -1) {
-                        var fld = placement.match(/prependParent\(([a-zA-Z0-9_\-.#]+)\)/);
-                        if (fld && fld.length) {
-                            $field.parent(fld[1]).prepend('<span class="error"><span>' + message + '</span></span>');
-                        } else {
-                            $field.parent().prepend('<span class="error"><span>' + message + '</span></span>');
-                        }
-                    } else if (placement == 'before') {
-                        $field.before('<span class="error"><span>' + message + '</span></span>');
-                    } else if (placement == 'after') {
-                        $field.after('<span class="error"><span>' + message + '</span></span>');
-                    }
+            message = $.errorHandler.message.replace('{message}', err[name]);
+            $field = form && form.length ? $('#' + form).find('#' + name) : $('#' + name);
+            if (!$field.length) {
+                $field = form && form.length ? $('#' + form).find('*[name=' + name + ']') : $('*[name=' + name + ']');
+            }
+            
+            if (!$field.length) {
+                continue;
+            }
+            
+            $field.addClass('error');
+            placement = $field.data('placement');
+            if (!placement) {
+                $field.after(message);
+                continue;
+            }
+            
+            if (placement.indexOf('appendClosest') > -1) {
+                fld = placement.match(/appendClosest\(([a-zA-Z0-9_\-.#]+)\)/);
+                if (fld && fld.length) {
+                    $field.closest(fld[1]).append(message);
                 } else {
-                    $field.after('<span class="error"><span>' + message + '</span></span>');
+                    $field.parent().append(message);
                 }
+            } else if (placement.indexOf('prependClosest') > -1) {
+                fld = placement.match(/prependClosest\(([a-zA-Z0-9_\-.#]+)\)/);
+                if (fld && fld.length) {
+                    $field.closest(fld[1]).prepend(message);
+                } else {
+                    $field.parent().prepend(message);
+                }
+            } else if (placement == 'appendForm') {
+                $field.closest('form').append(message);
+            } else if (placement == 'prependForm') {
+                $field.closest('form').prepend(message);
+            } else if (placement == 'appendSiblingLabel') {
+                $field.siblings('label').append(message);
+            } else if (placement == 'prependSiblingLabel') {
+                $field.siblings('label').prepend(message);
+            } else if (placement.indexOf('appendParent') > -1) {
+                fld = placement.match(/appendParent\(([a-zA-Z0-9_\-.#]+)\)/);
+                if (fld && fld.length) {
+                    $field.parent(fld[1]).append(message);
+                } else {
+                    $field.parent().append(message);
+                }
+            } else if (placement.indexOf('prependParent') > -1) {
+                fld = placement.match(/prependParent\(([a-zA-Z0-9_\-.#]+)\)/);
+                if (fld && fld.length) {
+                    $field.parent(fld[1]).prepend(message);
+                } else {
+                    $field.parent().prepend(message);
+                }
+            } else if (placement == 'before') {
+                $field.before(message);
+            } else if (placement == 'after') {
+                $field.after(message);
             }
         }
-    }
+    };
+    
+    // allow for overriding of error format
+    $.errorHandler.format = '<span class="error"><span>{message}</span></span>';
 
     // remove error box on focus
     $('form .error').live('focus', function() {
